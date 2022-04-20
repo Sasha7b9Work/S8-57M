3a45100bcd1b5a70de4a91fbfc419413fb3ef50c
Sasha7b9 <Sasha7b9@tut.by>
2022-04-20 14:45:00 +0300

    *

--- sources/scripts/vs_keil/make.bat
+++ sources/scripts/vs_keil/make.bat
@@ -21,7 +21,7 @@ cd ../..
 rmdir "generated/Keil" /s /q
 cd scripts/vs_keil
 @echo on
-cmake ../../CMakeLists.txt -B../../generated/Keil -G "Visual Studio 16 2019" -A Win32
+cmake ../../CMakeLists.txt -B../../generated/Keil -G "Visual Studio 17 2022" -A Win32
 
 @echo off
 
