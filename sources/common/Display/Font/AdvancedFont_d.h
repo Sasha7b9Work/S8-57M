7b393ef6c4ea90560f483b7cd3f02415ad005f65
Sasha7b9 <Sasha7b9@tut.by>
2022-04-21 10:04:27 +0300

    *

--- sources/common/Display/Font/AdvancedFont_d.h
+++ sources/common/Display/Font/AdvancedFont_d.h
@@ -6,19 +6,25 @@
 struct DAdvancedFont
 {
     DAdvancedFont() {};
+
     // Установить действующий шрифт
     DAdvancedFont(DTypeFont::E type);
+
     // Возвращает true, если в строке row есть пиксели
     bool RowNotEmpty(uint8 symbol, int row);
+
     // Возвращает ширину символа
     uint8 GetWidth(uint8 symbol);
+
     // Возвращает высоту символа
     uint8 GetHeight(uint8 symbol);
+
     // Возвращает высоту самого высокого символа
     uint8 GetHeight();
 
     bool BitIsExist(uint8 symbol, int row, int bit);
 
 private:
+
     static DTypeFont::E currentType;
 };
