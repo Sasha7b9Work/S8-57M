b75a5d94578f121ec014fd2051a194e3c1f8f491
Sasha7b9 <Sasha7b9@tut.by>
2022-04-21 08:21:24 +0300

    *

--- sources/Device/src/Hardware/Memory/ExtRAM.h
+++ sources/Device/src/Hardware/Memory/ExtRAM.h
@@ -1,31 +1,29 @@
 #pragma once
 
 
-class ExtRAM
+namespace ExtRAM
 {
-public:
-    
     // Первый байт внешней памяти
-    static uint8 *Begin();
-    
-    // За последним байтом внешней памяти
-    static uint8 *End();
-    
-    // Записывает во внешнюю память по адресу address данные buffer, size кратен 4
-    static void Write(const uint8 *buffer, int size, uint8 *address);
-    
-    // Читает из внешней памяти address в буфер buffer данные, size кратен 4
-    static void Read(uint8 *buffer, int size, const uint8 *address);
+    uint8* Begin();
 
-    static void Fill(uint8 *begin = 0, uint8 value = 0, int size = 0);
+    // За последним байтом внешней памяти
+    uint8* End();
+
+    // Записывает во внешнюю память по адресу address данные buffer, size кратен 4
+    void Write(const uint8* buffer, int size, uint8* address);
+
+    // Читает из внешней памяти address в буфер buffer данные, size кратен 4
+    void Read(uint8* buffer, int size, const uint8* address);
+
+    void Fill(uint8* begin = 0, uint8 value = 0, int size = 0);
 
     // Возвращает сколько процентов данных считанных неправильно.
-    static float Test1();
-    static float Test2();
+    float Test1();
+    float Test2();
 
     // Возвращает время в секундах, которое занимает запись/чтение sizekB килобайт во внешнюю память. Возвращает -1, если произошла ошибка
-    static float TestTime(int sizekB);
-    
+    float TestTime(int sizekB);
+
     // Тест скорости записи/чтение 1кБ внешней RAM
-    static float TestTime1kB(uint8 *address);
+    float TestTime1kB(uint8* address);
 };
