96ad086d086271c3de40b7bd0f3d521c59677588
Sasha7b9 <Sasha7b9@tut.by>
2022-04-21 13:07:51 +0300

    *

--- sources/Device/src/Display/Painter_stm.cpp
+++ sources/Device/src/Display/Painter_stm.cpp
@@ -12,8 +12,15 @@ namespace Painter
 {
     static const int SIZE_BUFFER = Display::WIDTH * Display::HEIGHT / 2;
 
-    uint8 buffer[SIZE_BUFFER];
-    uint8 front[SIZE_BUFFER];
+    uint8 buffer1[SIZE_BUFFER];
+    uint8 buffer2[SIZE_BUFFER];
+
+    uint8 *prev = nullptr;      // Указатель на буфер, отрисованный в прошлом кадре
+    uint8 *draw = nullptr;      // В этом буфере рисуем в текущем буфере
+                                // Используетя для расчёта дельты перед передачей в дисплей
+
+    // Подготовить буфера к новому кадру
+    void ToggleBuffers();
 }
 
 
@@ -24,19 +31,43 @@ void Painter::Init()
 
 void Painter::BeginScene(Color color)
 {
+    ToggleBuffers();
+
     color.SetAsCurrent();
 
-    std::memset(buffer, Color::GetCurent().value, SIZE_BUFFER);
+    std::memset(draw, Color::GetCurent().value, SIZE_BUFFER);
 }
 
 
 void Painter::EndScene()
 {
-    // todo_paint
+    if (prev == nullptr)    // Нет предыдущей отрисовки - передаём нарисованный буфер
+    {
+
+    }
+    else                    // Если есть буфер, отрисованный в прошлом кадре, то пересылаем дельту
+    {
+
+    }
 }
 
 
 void Painter::DrawTesterData(uint8 , Color , const uint16 *, const uint8 *)
 {
     // todo_paint
+}
+
+
+void Painter::ToggleBuffers()
+{
+    if (prev == nullptr && draw == nullptr)
+    {
+        draw = buffer1;
+    }
+    else
+    {
+        prev = draw;
+
+        draw = (prev == buffer1) ? buffer2 : buffer1;
+    }
 }
