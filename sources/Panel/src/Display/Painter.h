73a8d2f3bf7dfc401379974f4b74be4a06eb30be
Sasha7b9 <Sasha7b9@tut.by>
2022-04-22 13:27:08 +0300

    *

--- sources/Panel/src/Display/Painter.h
+++ sources/Panel/src/Display/Painter.h
@@ -11,24 +11,24 @@ struct Painter
 {
     // Вызывается в конце отрисовки каждого кадра. Переносит содержимое буфера на экран
     static void EndScene();
-    
+
     // Пересылает строку в панель
     static void SendRow(int row);
 
     static void SetColor(Color color);
-    
+
     // Возвращает текущий цвет рисования
     static Color GetColor();
-    
+
     // Устанавливает цвету заданное значение. Загружается в дисплей LoadPalette() или SetPalette()
     static void SetColorValue(Color color, uint value);
-    
+
     // Загружает в дисплей все цвета
     static void LoadPalette();
 
     // Нарисовать данные тестер-компонента. В младшем бите - линиями или точками рисовать. В старшей тетраде - количество усреднений
     static void DrawTesterData(uint8 mode, Color color, const uint16 x[TESTER_NUM_POINTS], const uint8 y[TESTER_NUM_POINTS]);
-    
+
     // Установить цвет рисования
     // Нарисовать точку текущим цветом
     static void SetPoint(int x, int y);
@@ -36,20 +36,20 @@ struct Painter
     static void DrawVPointLine(int x, int y, int delta, int count);
 
     static void DrawHPointLine(int x, int y, int delta, int count);
-    
+
     // Нарисовать горизонтальную линию
     static void DrawHLine(int y, int x0, int x1);
-    
+
     // Нарисовать вертикальную линию
     static void DrawVLine(int x, int y0, int y1);
-    
+
     // Нарисовать произвольную линию
     static void DrawLine(int x0, int y0, int x1, int y1);
-    
+
     // \brief Рисует прерывистую горизонтальную линию. dFill - длина штриха, dEmpty - расст. между штрихами. Линия всегда начинается со штриха. 
     // dStart указывает смещение первой рисуемой точки относительно начала штриха.
     static void DrawDashedHLine(int y, int x0, int x1, int dFill, int dEmpty, int dStart);
-    
+
     // Рисует прерывистую вертикальную линию.
     static void DrawDashedVLine(int x, int y0, int y1, int dFill, int dEmpty, int dStart);
 
@@ -59,11 +59,11 @@ struct Painter
 
     static uint ReduceBrightness(uint colorValue, float newBrightness);
 
-    static int DrawFormatText(int x, int y, char *format, ...);
-    
+    static int DrawFormatText(int x, int y, char* format, ...);
+
     // Пишет строку в позиции x, y
     static int DrawFormText(int x, int y, Color color, pString text, ...);
-    
+
     // Посылает строку изображения numString в устройство
     static void SendScreenToDevice();
 };
