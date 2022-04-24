6201ddd3e2d1fa334b0437e8b3bad8f1d69e28f7
Sasha7b9 <Sasha7b9@tut.by>
2022-04-24 21:02:07 +0300

    *

---
+++ sources/Panel/src/Utils/StringUtils.cpp
@@ -0,0 +1,9 @@
+// 2022/4/24 13:21:56 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Utils/StringUtils.h"
+
+
+uint8 *SU::FindSequency(uint8 *begin, uint8 *end, uint8 *sequency_begin, uint8 *sequency_end)
+{
+    return nullptr;
+}
