f101db9df97c93898053ef9c114e30a6a927d0fd
Sasha7b9Work <Phuntik-it@tut.by>
2022-11-29 15:45:20 +0300

    *

--- sources/Panel/src/Display/Painter.h
+++ sources/Panel/src/Display/Painter.h
@@ -6,63 +6,63 @@
 #include "Display/Font/Font_p.h"
 
 
-struct Painter
+namespace Painter
 {
     // Вызывается в начале отрисовки каждого кадра. Заполняет буфер цветом color
-    static void BeginScene();
-    
-    // Вызывается в конце отрисовки каждого кадра. Переносит содержимое буфера на экран
-    static void EndScene();
-    
-    // Пересылает строку в панель
-    static void SendRow(int row);
+    void BeginScene();
 
-    static void SetColor(Color color);
-    
+    // Вызывается в конце отрисовки каждого кадра. Переносит содержимое буфера на экран
+    void EndScene();
+
+    // Пересылает строку в панель
+    void SendRow(int row);
+
+    void SetColor(Color color);
+
     // Возвращает текущий цвет рисования
-    static Color GetColor();
-    
+    Color GetColor();
+
     // Устанавливает цвету заданное значение. Загружается в дисплей LoadPalette() или SetPalette()
-    static void SetColorValue(Color color, uint value);
-    
+    void SetColorValue(Color color, uint value);
+
     // Загружает в дисплей все цвета
-    static void LoadPalette();
+    void LoadPalette();
 
     // Установить цвет рисования
     // Нарисовать точку текущим цветом
-    static void SetPoint(int x, int y);
+    void SetPoint(int x, int y);
 
-    static void DrawVPointLine(int x, int y, int delta, int count);
+    void DrawVPointLine(int x, int y, int delta, int count);
 
-    static void DrawHPointLine(int x, int y, int delta, int count);
-    
+    void DrawHPointLine(int x, int y, int delta, int count);
+
     // Нарисовать горизонтальную линию
-    static void DrawHLine(int y, int x0, int x1);
-    
+    void DrawHLine(int y, int x0, int x1);
+
     // Нарисовать вертикальную линию
-    static void DrawVLine(int x, int y0, int y1);
-    
+    void DrawVLine(int x, int y0, int y1);
+
     // Нарисовать произвольную линию
-    static void DrawLine(int x0, int y0, int x1, int y1);
-    
+    void DrawLine(int x0, int y0, int x1, int y1);
+
     // \brief Рисует прерывистую горизонтальную линию. dFill - длина штриха, dEmpty - расст. между штрихами. Линия всегда начинается со штриха. 
     // dStart указывает смещение первой рисуемой точки относительно начала штриха.
-    static void DrawDashedHLine(int y, int x0, int x1, int dFill, int dEmpty, int dStart);
-    
+    void DrawDashedHLine(int y, int x0, int x1, int dFill, int dEmpty, int dStart);
+
     // Рисует прерывистую вертикальную линию.
-    static void DrawDashedVLine(int x, int y0, int y1, int dFill, int dEmpty, int dStart);
+    void DrawDashedVLine(int x, int y0, int y1, int dFill, int dEmpty, int dStart);
 
-    static void DrawRectangle(int x, int y, int width, int height);
+    void DrawRectangle(int x, int y, int width, int height);
 
-    static void FillRegion(int x, int y, int width, int height);
+    void FillRegion(int x, int y, int width, int height);
 
-    static uint ReduceBrightness(uint colorValue, float newBrightness);
+    uint ReduceBrightness(uint colorValue, float newBrightness);
 
-    static int DrawFormatText(int x, int y, char *format, ...);
-    
+    int DrawFormatText(int x, int y, char *format, ...);
+
     // Пишет строку в позиции x, y
-    static int DrawFormText(int x, int y, Color color, pString text, ...);
-    
+    int DrawFormText(int x, int y, Color color, pString text, ...);
+
     // Посылает строку изображения numString в устройство
-    static void SendScreenToDevice();
+    void SendScreenToDevice();
 };
--- sources/Panel/src/Display/Text.h
+++ sources/Panel/src/Display/Text.h
@@ -2,14 +2,14 @@
 #include "Display/Colors.h"
 
 
-class Text
+namespace Text
 {
-public:
-    static int Draw(int x, int y, const char *text);
+    int Draw(int x, int y, const char *text);
 
-    static int DrawChar(int x, int y, uint8 symbol, Color color = Color::NUMBER);
+    int DrawChar(int x, int y, uint8 symbol, Color color = Color::NUMBER);
+
     // ”становка моноширинного режима вывода текста
-    static void SetMinWidthFont(uint8 width);
+    void SetMinWidthFont(uint8 width);
 
-    static void SetSpacing(uint8 space);
+    void SetSpacing(uint8 space);
 };
