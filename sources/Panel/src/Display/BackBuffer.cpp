cab14303829ed1df23a45f05fbb29562cdb8ef9f
Sasha7b9Work <Phuntik-it@tut.by>
2022-12-06 15:35:31 +0300

    Сетка отрисована полностью

--- sources/Panel/src/Display/BackBuffer.cpp
+++ sources/Panel/src/Display/BackBuffer.cpp
@@ -8,6 +8,7 @@
 #include "Hardware/Timer.h"
 #include <cstdlib>
 #include <cstring>
+#include <cmath>
 
 
 namespace BackBuffer
@@ -151,17 +152,82 @@ void BackBuffer::Fill()
 
 void BackBuffer::DrawHPointLine(int x, int y, int delta, int count)
 {
-
+    for (int i = 0; i < count; i++)
+    {
+        SetPoint(x, y);
+        x += delta;
+    }
 }
 
 
 void BackBuffer::DrawVPointLine(int x, int y, int delta, int count)
 {
-
+    for (int i = 0; i < count; i++)
+    {
+        SetPoint(x, y);
+        y += delta;
+    }
 }
 
 
-void BackBuffer::DrawLine(int x0, int y0, int x1, int y1)
+void BackBuffer::DrawLine(int x1, int y1, int x2, int y2)
 {
+    if (x1 == x2 && y1 == y2)
+    {
+        SetPoint(x1, y1);
+        return;
+    }
+    else if (x1 == x2)
+    {
+        DrawVLine(x1, y1, y2);
+        return;
+    }
+    else if (y1 == y2) //-V2516
+    {
+        DrawHLine(y1, x1, x2);
+        return;
+    }
 
+    int x = x1;
+    int y = y1;
+    int dx = static_cast<int>(std::fabsf(static_cast<float>(x2 - x1)));
+    int dy = static_cast<int>(std::fabsf(static_cast<float>(y2 - y1)));
+    int s1 = Math::Sign(x2 - x1);
+    int s2 = Math::Sign(y2 - y1);
+    int temp;
+    int exchange = 0;
+    if (dy > dx)
+    {
+        temp = dx;
+        dx = dy;
+        dy = temp;
+        exchange = 1;
+    }
+    int e = 2 * dy - dx;
+    int i = 0;
+    for (; i <= dx; i++)
+    {
+        SetPoint(x, y);
+        while (e >= 0)
+        {
+            if (exchange)
+            {
+                x += s1;
+            }
+            else
+            {
+                y += s2;
+            }
+            e = e - 2 * dx;
+        }
+        if (exchange)
+        {
+            y += s2;
+        }
+        else
+        {
+            x += s1;
+        }
+        e = e + 2 * dy;
+    }
 }
