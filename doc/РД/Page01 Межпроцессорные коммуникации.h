87350696d6e1aaf97c5a92020933c9f09b8e6854
Sasha7b9Work <Phuntik-it@tut.by>
2022-12-06 16:56:50 +0300

    *

--- doc/РД/Page01 Межпроцессорные коммуникации.h
+++ doc/РД/Page01 Межпроцессорные коммуникации.h
@@ -2,11 +2,11 @@
 | №  | Наим. |    437ZIT         |           |   479VIT6        | у |   Ум - значение по умолчанию
 |конт|       |     FPGA          |           |   Панель         | м |
 +----+-------+--------------+----+-----------+-------------+----+---+
-|    |       | 43  PA7      | I  | PAN_READY | 49 PB15     | O  | 1 |Здесь выставляется признак готовности к коммуникации и признак подтверждения
-|    |       | 44  PC4      | I  | PAN_DATA  | 48 PB14     | O  | 0 |Здесь выставляется признак готовности данных для передачи в устройство
-|    |       | 127 PG12 CS  | O  | CS        | 50 PD8      | I  |   |По этому сигналу от основого МК начинаем транзакцию чтения/записи
-|    |       | 119 PD5  WR  | O  | WR        | 89 PD5  WR  | I  |   |Признак того, что основной МК осуществляет операцию записи в панель
-|    |       | 118 PD4  RD  | O  | RD        | 88 PD4  RD  | I  |   |Признак того, что основной МК осуществляет операцию чтения из панели
+|    |       | 43  PA7      | I  | PAN_READY | 49 PB15     | O  | 1 | Здесь выставляется признак готовности к коммуникации и признак подтверждения
+|    |       | 44  PC4      | I  | PAN_DATA  | 48 PB14     | O  | 0 | Здесь выставляется признак готовности данных для передачи в устройство
+|    |       | 127 PG12 CS  | O  | CS        | 50 PD8      | I  |   | Установленное в ноль значение означает, что МК хочет передать/принять данные
+|    |       | 119 PD5  WR  | O  | WR        | 89 PD5  WR  | I  |   | Установленное в ноль значение означает, что МК хочет записать данные
+|    |       | 118 PD4  RD  | O  | RD        | 88 PD4  RD  | I  |   | Признак того, что основной МК осуществляет операцию чтения из панели
 |    |       |              |    |           |             |    |   |
 |    | AD0   | 85  PD14 D0  | IO | D0        | 53 PD14 DA0 | IO |   |
 |    | AD1   | 86  PD15 D1  | IO | D1        | 54 PD15 DA1 | IO |   |
@@ -21,50 +21,211 @@
 
  *** Алогоритм записи/чтения ***
 
- Панель.
+ * Панель *
  
 void HAL_BUS::Update()
 {
-    while (pinCS.IsActive() == 0)
+    while(in_CS.IsActive())
     {
-        if (pinWR.IsActive() == 0)                  // Чтение байта из устройства
+        if (in_WR.IsActive())                           // Чтение байта из устройства
         {
             uint8 data = DataBus::ReadValue();
 
-            pinReady.SetPassive();
+            pin_ready.SetPassive();
 
-            PDecoder::AddData(data);        // \todo Сейчас недостаток - пока не отработает PDecoder::AddData(), устройство не пойдёт дальше
+            PDecoder::AddData(data);                    // \todo Сейчас недостаток - пока не отработает PDecoder::AddData(), устройство не пойдёт дальше
 
-            //while(pinCS.IsActive()) {};
-            //while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET) { }
-            volatile uint state = GPIOD->IDR & GPIO_PIN_8;
-            while (state == 0)
-            {
-                state = GPIOD->IDR & GPIO_PIN_8;
-            }
+            while(in_CS.IsActive()) {};
 
-            //pinReady.SetActive();
-            GPIOB->BSRR = (uint)GPIO_PIN_12 << 16U;
+            pin_ready.SetActive();
         }
-        else if (pinRD.IsActive() && queueData.Size())   // Запись байта в устройсто //-V2570 //-V2516
+        else if (in_RD.IsActive() && queueData.Size())   // Запись байта в устройсто //-V2570 //-V2516
         {
             CONFIG_TO_WRITE;
 
             // Устанавливаем данные на ШД
-            HAL_BUS::DataBus::WriteValue(queueData.Front());
+            DataBus::WriteValue(queueData.Front());
 
-            pinReady.SetPassive();
+            out_ready.SetPassive();
 
-            pinCS.WaitPassive();
+            in_CS.WaitPassive();
 
-            pinReady.SetActive();
+            out_ready.SetActive();
 
             if (queueData.Size() == 0)
             {
-                pinData.SetPassive();
+                out_data.SetPassive();
             }
 
             CONFIG_TO_READ;
+        }
+    }
+}
+
+
+ * Устройство *
+ 
+bool HAL_BUS::Panel::Receive() //-V2506
+{
+    //if(in_ready.IsPassive() || in_data.IsPassive())
+    //if((GPIOA->IDR & GPIO_PIN_7) != GPIO_PIN_RESET || (GPIOC->IDR & GPIO_PIN_4) != GPIO_PIN_RESET)
+    if ((GPIOA->IDR & GPIO_PIN_7) || (GPIOC->IDR & GPIO_PIN_4)) //-V2570
+    {
+        return false;
+    }
+
+    interactionWithPanel = true;
+
+    if (mode != Mode::PanelRead)
+    {
+        mode = Mode::PanelRead;
+
+        out_RD.Init();
+
+        // Конфигурируем ШД на чтение
+
+        GPIOD->MODER &= 0x0ffffff0U;        // Настроим пины 14, 15, 0, 1 на запись D0, D1, D2, D3
+
+        GPIOE->MODER &= 0xffc03fffU;        // Настроим пины 7, 8, 9, 10 на запись D4, D5, D6, D7
+    }
+
+    out_RD.SetActive();
+
+    out_CS.SetActive();
+
+    in_ready.WaitPassive();
+
+    while (in_ready.IsActive()) {};
+
+    uint8 data = 0;
+
+    while (in_ready.IsActive())
+    {
+        if (in_data.IsPassive())
+        {
+            goto exit;
+        }
+    }
+
+    //                                                 4,5,6,7              2,3                          0,1
+    data = static_cast<uint8>((GPIOE->IDR >> 3) & 0xF0 | (GPIOD->IDR << 2) & 0x0C | (GPIOD->IDR >> 14));
+
+    DDecoder::AddData(data);
+
+exit:
+
+    out_RD.SetPassive();
+
+    out_CS.SetPassive();
+
+    interactionWithPanel = false;
+
+    ///
+    // \todo Если убрать отсюда эти чтения, то нужно делать задержку, потому что без такой задержки зависает
+    // 
+
+    if (otherActionsAllowed)
+    {
+        if (Device::InModeRecorder())
+        {
+            Recorder::RecordPoints();
+        }
+        else if (OSCI_IN_MODE_P2P)
+        {
+            Roller::ReadPoint();
+        }
+        else
+        {
+            Timer::PauseOnTicks(10);
+        }
+    }
+    else
+    {
+        Timer::PauseOnTicks(10);
+    }
+
+    return true;
+}
+
+
+void HAL_BUS::Panel::Send(const uint8 *data, int size)
+{
+    if (!(GPIOA->IDR & GPIO_PIN_7) && !(GPIOC->IDR & GPIO_PIN_4)) //-V2570
+    {
+        while (Receive()) {}
+    }
+
+    interactionWithPanel = true;
+
+    if (mode != Mode::PanelWrite)
+    {
+        mode = Mode::PanelWrite;
+
+        out_WR.Init();
+
+        // Конфигурируем ШД на запись
+
+        GPIOD->MODER &= 0x0ffffff0U;        // Настроим пины 14, 15, 0, 1 на запись D0, D1, D2, D3
+        GPIOD->MODER |= 0x50000005U;        // Устанавливаем для этих пинов GPIO_MODE_OUTPUT_PP
+
+        GPIOE->MODER &= 0xffc03fffU;        // Настроим пины 7, 8, 9, 10 на запись D4, D5, D6, D7
+        GPIOE->MODER |= 0x00154000U;        // Устанавливаем для этих пинов GPIO_MODE_OUTPUT_PP
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        uint8 d = *data++;
+
+        //                                                                             биты 0,1                                 биты 2,3
+        GPIOD->ODR = (GPIOD->ODR & 0x3ffc) + static_cast<uint16>((static_cast<int16>(d) & 0x03) << 14) + ((static_cast<uint16>(d & 0x0c)) >> 2);  // Записываем данные в выходные пины
+        //                                                                          Биты 4,5,6,7
+        GPIOE->ODR = (GPIOE->ODR & 0xf87f) + static_cast<uint16>((static_cast<int16>(d) & 0xf0) << 3);
+
+        //out_WR.SetActive();                  // Даём сигнал записи
+        GPIOD->BSRR = (uint)GPIO_PIN_5 << 16U;
+
+        //while(in_ready.IsPassive()) {}   // И ожидаем сигнал панели о том, что она свободна
+        //while(HAL_PIO::Read(HPort::_A, HPin::_7) == 1) { };
+        volatile uint state = GPIOA->IDR & GPIO_PIN_7;
+        while (state)
+        {
+            state = GPIOA->IDR & GPIO_PIN_7;
+        }
+
+        //out_CS.SetActive();                  // Даём признак того, чта данные выставлены и можно их считывать
+        GPIOG->BSRR = (uint)GPIO_PIN_12 << 16U;
+
+        //while(in_ready.IsActive()) {}    // Переключение PIN_PAN_READY в неактивное состояние означает, что панель приняла данные и обрабатывает их
+        state = GPIOA->IDR & GPIO_PIN_7;
+        while (state == 0)
+        {
+            state = GPIOA->IDR & GPIO_PIN_7;
+        }
+
+        //out_WR.SetPassive();                 // \ Устанавливаем WR и CS в неактивное состояние - элементарный цикл записи окончен
+        GPIOD->BSRR = GPIO_PIN_5;
+
+        //out_CS.SetPassive();                 // /
+        GPIOG->BSRR = GPIO_PIN_12;
+    }
+
+    interactionWithPanel = false;
+
+    if (otherActionsAllowed)
+    {
+        if (Device::InModeRecorder())
+        {
+            Recorder::RecordPoints();
+        }
+        else if (OSCI_IN_MODE_P2P) //-V2516
+        {
+            Roller::ReadPoint();
+        }
+
+        if (funcAfterInteraction)
+        {
+            funcAfterInteraction();
+            funcAfterInteraction = nullptr;
         }
     }
 }
--- sources/Device/src/Hardware/HAL/HAL_FSMC_PAN_stm.cpp
+++ sources/Device/src/Hardware/HAL/HAL_FSMC_PAN_stm.cpp
@@ -18,8 +18,8 @@ struct OutPin
 {
     OutPin(HPort::E _port, uint16 _pin) : port(_port), pin(_pin) {}
 
-    void Init()       { HAL_PIO::Init(port, pin, HMode::Output_PP, HPull::Up); SetPassive(); }
-    void SetActive()  { HAL_PIO::Reset(port, pin); }
+    void Init() { HAL_PIO::Init(port, pin, HMode::Output_PP, HPull::Up); SetPassive(); }
+    void SetActive() { HAL_PIO::Reset(port, pin); }
     void SetPassive() { HAL_PIO::Set(port, pin); }
 
     HPort::E port;
@@ -31,11 +31,11 @@ struct InPin
 {
     InPin(HPort::E _port, uint16 _pin) : port(_port), pin(_pin) {}
 
-    void Init()        { HAL_PIO::Init(port, pin, HMode::Input, HPull::Up); }
-    bool IsActive()    { return HAL_PIO::Read(port, pin) == 0; };
-    bool IsPassive()   { return HAL_PIO::Read(port, pin) == 1; };
-    void WaitActive()  { while(IsPassive()) { } }
-    void WaitPassive() { while(IsActive()) { } }
+    void Init() { HAL_PIO::Init(port, pin, HMode::Input, HPull::Up); }
+    bool IsActive() { return HAL_PIO::Read(port, pin) == 0; };
+    bool IsPassive() { return HAL_PIO::Read(port, pin) == 1; };
+    void WaitActive() { while (IsPassive()) {} }
+    void WaitPassive() { while (IsActive()) {} }
 
     HPort::E port;
     uint16 pin;
@@ -51,7 +51,7 @@ struct DataBus
 
 static bool interactionWithPanel = false;       // true означает, что шина находится в процессе обмена с панелью и запись по обычной FSMC в альтеру и память запрещена
 static bool otherActionsAllowed = true;         // true означает, что во время приёма/передачи разрешены и другие действия, выполняемые в процессе транзакция - чтение
-                                                // точек в поточечном режиме и регистраторе
+// точек в поточечном режиме и регистраторе
 
 static OutPin out_CS(PIN_CS);                   // По активному состоянию этого пина панель определяет, что главный МК готов к взаимодействию
 static OutPin out_WR(PIN_WR);                   // По активновному состоянию этого пина панель определяте, что главный МК хочет делать передачу
@@ -81,14 +81,14 @@ bool HAL_BUS::Panel::Receive() //-V2506
 {
     //if(in_ready.IsPassive() || in_data.IsPassive())
     //if((GPIOA->IDR & GPIO_PIN_7) != GPIO_PIN_RESET || (GPIOC->IDR & GPIO_PIN_4) != GPIO_PIN_RESET)
-    if((GPIOA->IDR & GPIO_PIN_7) || (GPIOC->IDR & GPIO_PIN_4)) //-V2570
+    if ((GPIOA->IDR & GPIO_PIN_7) || (GPIOC->IDR & GPIO_PIN_4)) //-V2570
     {
         return false;
     }
-   
+
     interactionWithPanel = true;
-    
-    if(mode != Mode::PanelRead)
+
+    if (mode != Mode::PanelRead)
     {
         mode = Mode::PanelRead;
 
@@ -100,49 +100,49 @@ bool HAL_BUS::Panel::Receive() //-V2506
 
         GPIOE->MODER &= 0xffc03fffU;        // Настроим пины 7, 8, 9, 10 на запись D4, D5, D6, D7
     }
-    
+
     out_RD.SetActive();
 
     out_CS.SetActive();
-    
+
     in_ready.WaitPassive();
 
-    while(in_ready.IsActive()) {};
-    
+    while (in_ready.IsActive()) {};
+
     uint8 data = 0;
 
-    while(in_ready.IsActive())
+    while (in_ready.IsActive())
     {
-        if(in_data.IsPassive())
+        if (in_data.IsPassive())
         {
             goto exit;
         }
     }
-    
+
     //                                                 4,5,6,7              2,3                          0,1
     data = static_cast<uint8>((GPIOE->IDR >> 3) & 0xF0 | (GPIOD->IDR << 2) & 0x0C | (GPIOD->IDR >> 14));
 
     DDecoder::AddData(data);
-    
+
 exit:
-    
+
     out_RD.SetPassive();
 
     out_CS.SetPassive();
-    
+
     interactionWithPanel = false;
 
     ///
     // \todo Если убрать отсюда эти чтения, то нужно делать задержку, потому что без такой задержки зависает
     // 
 
-    if(otherActionsAllowed)
+    if (otherActionsAllowed)
     {
-        if(Device::InModeRecorder())
+        if (Device::InModeRecorder())
         {
             Recorder::RecordPoints();
         }
-        else if(OSCI_IN_MODE_P2P)
+        else if (OSCI_IN_MODE_P2P)
         {
             Roller::ReadPoint();
         }
@@ -169,20 +169,20 @@ void HAL_BUS::Panel::Send(uint8 byte)
 void HAL_BUS::Panel::Send(uint8 byte0, uint8 byte1)
 {
     uint8 buffer[2] = { byte0, byte1 };
-    
+
     Send(buffer, 2);
 }
 
 void HAL_BUS::Panel::Send(const uint8 *data, int size)
 {
-    if(!(GPIOA->IDR & GPIO_PIN_7) && !(GPIOC->IDR & GPIO_PIN_4)) //-V2570
+    if (!(GPIOA->IDR & GPIO_PIN_7) && !(GPIOC->IDR & GPIO_PIN_4)) //-V2570
     {
-        while(Receive()) { }
+        while (Receive()) {}
     }
 
     interactionWithPanel = true;
 
-    if(mode != Mode::PanelWrite)
+    if (mode != Mode::PanelWrite)
     {
         mode = Mode::PanelWrite;
 
@@ -197,7 +197,7 @@ void HAL_BUS::Panel::Send(const uint8 *data, int size)
         GPIOE->MODER |= 0x00154000U;        // Устанавливаем для этих пинов GPIO_MODE_OUTPUT_PP
     }
 
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         uint8 d = *data++;
 
@@ -208,11 +208,11 @@ void HAL_BUS::Panel::Send(const uint8 *data, int size)
 
         //out_WR.SetActive();                  // Даём сигнал записи
         GPIOD->BSRR = (uint)GPIO_PIN_5 << 16U;
-        
+
         //while(in_ready.IsPassive()) {}   // И ожидаем сигнал панели о том, что она свободна
         //while(HAL_PIO::Read(HPort::_A, HPin::_7) == 1) { };
         volatile uint state = GPIOA->IDR & GPIO_PIN_7;
-        while(state)
+        while (state)
         {
             state = GPIOA->IDR & GPIO_PIN_7;
         }
@@ -222,7 +222,7 @@ void HAL_BUS::Panel::Send(const uint8 *data, int size)
 
         //while(in_ready.IsActive()) {}    // Переключение PIN_PAN_READY в неактивное состояние означает, что панель приняла данные и обрабатывает их
         state = GPIOA->IDR & GPIO_PIN_7;
-        while(state == 0)
+        while (state == 0)
         {
             state = GPIOA->IDR & GPIO_PIN_7;
         }
@@ -236,18 +236,18 @@ void HAL_BUS::Panel::Send(const uint8 *data, int size)
 
     interactionWithPanel = false;
 
-    if(otherActionsAllowed)
+    if (otherActionsAllowed)
     {
-        if(Device::InModeRecorder())
+        if (Device::InModeRecorder())
         {
             Recorder::RecordPoints();
         }
-        else if(OSCI_IN_MODE_P2P) //-V2516
+        else if (OSCI_IN_MODE_P2P) //-V2516
         {
             Roller::ReadPoint();
         }
 
-        if(funcAfterInteraction)
+        if (funcAfterInteraction)
         {
             funcAfterInteraction();
             funcAfterInteraction = nullptr;
