5bf87e3889cc0118ae77e3fd0c94da9473ac1719
Sasha7b9Work <Phuntik-it@tut.by>
2023-01-09 16:01:42 +0300

    detrash

--- sources/Panel/src/Display/Colors.h
+++ sources/Panel/src/Display/Colors.h
@@ -28,22 +28,11 @@ private:
     static Color current;
 };
 
+
 bool operator!=(const Color &left, const Color &right);
 bool operator==(const Color &left, const Color &right);
 bool operator>(const Color &left, const Color &right);
 
-#ifdef _WIN32
-#pragma warning(push)
-#pragma warning(disable : 4623)
-#endif
-
-
-#define DEF_COLOR_TYPE(name, r, g, b, sR, sG, sB, bright, col) ColorType name = {r, g, b, sR, sG, sB, bright, col, 0, false, false};
-#define COLOR_TYPE(r, g, b, sR, sG, sB, bright, col) {r, g, b, sR, sG, sB, bright, col, 0, false, false};
-
-#ifdef _WIN32
-#pragma warning(pop)
-#endif
 
 #define MAKE_COLOR(r, g, b) ((uint)((uint)(b) + ((uint)(g) << 8) + ((uint)(r) << 16)))
 #define R_FROM_COLOR(color) (((uint)(color) >> 16) & 0xff)
