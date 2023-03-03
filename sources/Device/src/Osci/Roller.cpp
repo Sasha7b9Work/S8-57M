2fee15f380bfa57f870963bdfbb5b7c6c0c2f5ed
Sasha7b9Work <Phuntik-it@tut.by>
2023-03-03 13:29:33 +0300

    Исправил чтение пикового детектора в поточечном режиме, но всё равно рисует неправильно

--- sources/Device/src/Osci/Roller.cpp
+++ sources/Device/src/Osci/Roller.cpp
@@ -53,10 +53,14 @@ void Roller::ReadPoint()
     if (FPGA::IsRunning() && (HAL_PIO::Read(PIN_P2P) != 0))
     {
         HAL_BUS::FPGA::SetAddrData(RD::DATA_A, RD::DATA_A + 1);
-        BitSet16 dataA(HAL_BUS::FPGA::ReadA0(), HAL_BUS::FPGA::ReadA1());
+        uint8 d1 = HAL_BUS::FPGA::ReadA1();
+        uint8 d0 = HAL_BUS::FPGA::ReadA0();
+        BitSet16 dataA(d1, d0);
     
         HAL_BUS::FPGA::SetAddrData(RD::DATA_B, RD::DATA_B + 1);
-        BitSet16 dataB(HAL_BUS::FPGA::ReadA0(), HAL_BUS::FPGA::ReadA1());
+        d1 = HAL_BUS::FPGA::ReadA1();
+        d0 = HAL_BUS::FPGA::ReadA0();
+        BitSet16 dataB(d1, d0);
     
         addPoint(dataA, dataB);
     }
