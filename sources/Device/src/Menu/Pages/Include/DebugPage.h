ec02753ea03b388d3658d9ca72595e6ca7226194
Sasha7b9Work <Phuntik-it@tut.by>
2023-03-06 15:54:10 +0300

    refact

--- sources/Device/src/Menu/Pages/Include/DebugPage.h
+++ sources/Device/src/Menu/Pages/Include/DebugPage.h
@@ -3,42 +3,42 @@
 #include "Menu/MenuItems.h"
 
 
-#define S_DBG_SHOW_CONSOLE              (set.dbg._showConsole)
-#define S_DBG_NUM_STRINGS_IN_CONSOLE    (set.dbg._numStrings)
-#define S_DBG_SHOW_ALL_REGISTERS        (set.dbg._showAll)
-#define S_DBG_SHOW_FLAG                 (set.dbg._showFlag)
-#define S_DBG_SHOW_RSHIFT(chan)         (set.dbg._showRShift[chan])
-#define S_DBG_SHOW_TRIG_LEV             (set.dbg._showTrigLev)
-#define S_DBG_SHOW_RANGE(chan)          (set.dbg._showRange[chan])
-#define S_DBG_SHOW_CHAN_PARAM(chan)     (set.dbg._showChanParam[chan])
-#define S_DBG_SHOW_TRIG_PARAM           (set.dbg._showTrigParam)
-#define S_DBG_SHOW_TSHIFT               (set.dbg._showTShift)
-#define S_DBG_SHOW_TBASE                (set.dbg._showTBase)
-#define S_DBG_SHOW_STATS                (set.dbg._showStats)
-#define S_DBG_RUN_TEST                  (set.dbg._runTest)
-#define S_DBG_SHOW_BATTERY              (set.dbg._showBattery)
-#define S_DBG_SHOW_RAND_GATES           (set.dbg._showRandGate)
-#define S_DBG_SHOW_RAND_PRED_POST       (set.dbg._showRandPredPost)
+#define S_DBG_SHOW_CONSOLE              (set.dbg.showConsole)
+#define S_DBG_NUM_STRINGS_IN_CONSOLE    (set.dbg.numStrings)
+#define S_DBG_SHOW_ALL_REGISTERS        (set.dbg.showAll)
+#define S_DBG_SHOW_FLAG                 (set.dbg.showFlag)
+#define S_DBG_SHOW_RSHIFT(chan)         (set.dbg.showRShift[chan])
+#define S_DBG_SHOW_TRIG_LEV             (set.dbg.showTrigLev)
+#define S_DBG_SHOW_RANGE(chan)          (set.dbg.showRange[chan])
+#define S_DBG_SHOW_CHAN_PARAM(chan)     (set.dbg.showChanParam[chan])
+#define S_DBG_SHOW_TRIG_PARAM           (set.dbg.showTrigParam)
+#define S_DBG_SHOW_TSHIFT               (set.dbg.showTShift)
+#define S_DBG_SHOW_TBASE                (set.dbg.showTBase)
+#define S_DBG_SHOW_STATS                (set.dbg.showStats)
+#define S_DBG_RUN_TEST                  (set.dbg.runTest)
+#define S_DBG_SHOW_BATTERY              (set.dbg.showBattery)
+#define S_DBG_SHOW_RAND_GATES           (set.dbg.showRandGate)
+#define S_DBG_SHOW_RAND_PRED_POST       (set.dbg.showRandPredPost)
 
 
 struct SettingsDebug
 { //-V802
-    int8   _showConsole;         // Показывать ли консоль
-    int16  _numStrings;          // Число строк в консоли.
-    bool   _showAll;
-    bool   _showFlag;
-    bool   _showRShift[2];
-    bool   _showTrigLev;
-    bool   _showRange[2];
-    bool   _showChanParam[2];
-    bool   _showTrigParam;
-    bool   _showTShift;
-    bool   _showTBase;
-    bool   _showStats;           // Показывать статистику на экране (fps, например).
-    bool   _runTest;             // Если true, то нужно выполнять тест при запуске
-    bool   _showBattery;         // Показывать или нет состояние батареи
-    bool   _showRandGate;        // Выводить ли значения ворот в режиме рандомизатора
-    bool   _showRandPredPost;    // Выводить ли значения пред- и после- запуска в режиме рандомизатора
+    int8   showConsole;         // Показывать ли консоль
+    int16  numStrings;          // Число строк в консоли.
+    bool   showAll;
+    bool   showFlag;
+    bool   showRShift[2];
+    bool   showTrigLev;
+    bool   showRange[2];
+    bool   showChanParam[2];
+    bool   showTrigParam;
+    bool   showTShift;
+    bool   showTBase;
+    bool   showStats;           // Показывать статистику на экране (fps, например).
+    bool   runTest;             // Если true, то нужно выполнять тест при запуске
+    bool   showBattery;         // Показывать или нет состояние батареи
+    bool   showRandGate;        // Выводить ли значения ворот в режиме рандомизатора
+    bool   showRandPredPost;    // Выводить ли значения пред- и после- запуска в режиме рандомизатора
 };
 
 
